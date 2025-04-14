5663018702220744807a7a761eab9815804d5abe
oksigenasu@yahoo.com <oksigenasu@yahoo.com>
2025-04-14 16:32:46 +0700

    Quartz sync: Apr 14, 2025, 4:32 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Oksi's Blog",
+    pageTitle: "💻 Oksi's Blog",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
